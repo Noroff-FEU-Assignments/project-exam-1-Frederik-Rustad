d7604a1b6ef8e5561b47c5258bff2e9a5345679b
Frederik-Rustad <frederik.rustad.89@gmail.com>
2023-09-06 14:29:55 +0200

    Update caousel.js
    carousel now loops !!!

--- JS/caousel.js
+++ JS/caousel.js
@@ -55,19 +55,26 @@
 const prevBtn = document.getElementById('prevBtn');
 const nextBtn = document.getElementById('nextBtn');
 const carouselWrapper = document.querySelector('.carousel-wrapper');
+const carouselItems = document.querySelectorAll('.carousel-item');
 
 let currentPosition = 0;
 
 nextBtn.addEventListener('click', () => {
-    if (currentPosition > -200) {
-        currentPosition -= 33.33; 
-        carouselWrapper.style.transform = `translateX(${currentPosition}%)`;
+    currentPosition -= 33.33;
+    if (currentPosition < -33.33 * (carouselItems.length - 3)) {
+        currentPosition = 0;
     }
+    updateCarousel();
 });
 
 prevBtn.addEventListener('click', () => {
-    if (currentPosition < 0) {
-        currentPosition += 33.33; 
-        carouselWrapper.style.transform = `translateX(${currentPosition}%)`;
+    currentPosition += 33.33;
+    if (currentPosition > 0) {
+        currentPosition = -33.33 * (carouselItems.length - 3);
     }
+    updateCarousel();
 });
+
+function updateCarousel() {
+    carouselWrapper.style.transform = `translateX(${currentPosition}%)`;
+}
