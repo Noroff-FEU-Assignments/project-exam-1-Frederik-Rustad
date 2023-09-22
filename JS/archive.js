22c9efbed0672e3b48abcbc8d85fc2de6c0a5b39
Frederik-Rustad <frederik.rustad.89@gmail.com>
2023-09-22 13:16:38 +0200

    Update archive.js
    Loading more posts no longer takes you to the top of the page when posts are loaded.

--- JS/archive.js
+++ JS/archive.js
@@ -9,31 +9,30 @@ function getPosts() {
   let postsLoaded = 0;
   let allPosts = [];
 
-  const fetchArchivePosts = () => {
-    postContainer.innerHTML = '<p>Loading...</p>';
-    fetch(apiBlogs + `?offset=${postsLoaded}&per_page=10`)
+  const fetchPosts = (offset, count) => {
+    fetch(apiBlogs + `?offset=${offset}&per_page=${count}`)
       .then(response => response.json())
       .then(data => {
-        postContainer.innerHTML = '';
         data.forEach(post => {
           const html = `<a class='post archive' href='blogArticle.html?id=${post.id}'>
             <img src="${post.jetpack_featured_media_url}" class="postImg">
             <p class="date">${post.date}</p>
             <h3>${post.title.rendered}</h3>    
-          
           </a>`;
-          allPosts.push(html);        
-        });      
-        postContainer.innerHTML += allPosts.join('');
-        postsLoaded += 10;        
+          allPosts.push(html);
+        });
+        postContainer.innerHTML = allPosts.join('');
+        postsLoaded += count;
       })
       .catch(error => {
         console.error("Error fetching posts:", error);
         postContainer.innerHTML = '<p>Error fetching posts. Please refresh or try again later.</p>';
       });
   };
-  loadMoreButton.addEventListener('click', fetchArchivePosts);
-  fetchArchivePosts();
+  loadMoreButton.addEventListener('click', () => {
+    fetchPosts(postsLoaded, 10);
+  });
+  fetchPosts(postsLoaded, 10);
 }
 
 getPosts();
