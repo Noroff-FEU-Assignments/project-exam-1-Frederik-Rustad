b9d51c7e835a4bff2ae57a2fd0565c7993749f5d
Frederik-Rustad <frederik.rustad.89@gmail.com>
2023-09-25 21:40:46 +0200

    gallery modal
    added modal for gallery images

---
+++ JS/gallery.js
@@ -0,0 +1,25 @@
+      //modal
+      const imageElements = document.querySelectorAll('.gallery-image');
+      const modalClass = 'gallery-modal';
+      
+      imageElements.forEach(imageElement => {
+        imageElement.addEventListener('click', (event) => {
+          event.stopPropagation();
+          console.log('image clicked');
+          imageElement.classList.add(modalClass);      
+       
+          document.addEventListener('click', (e) => {
+            if (!isImageInsideModal(e.target)) {            
+              imageElement.classList.remove(modalClass);
+            }
+          });
+      
+          imageElement.addEventListener('click', () => {
+            imageElement.classList.toggle(modalClass);
+          });
+        });
+      });
+      function isImageInsideModal(element) {
+        return element.tagName === 'IMG' && element.closest('figure.modal');
+      } 
+      //modal end 
